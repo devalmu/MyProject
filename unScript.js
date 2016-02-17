629afa18826175a809c9406d73a95da186e1e171
val <vmulunda@gmail.com>
2016-02-17 16:10:05 +0100

    Petite modification au fichier

--- unScript.js
+++ unScript.js
@@ -2,4 +2,11 @@ var a; // déclaration d'une variable
 a = 3.14; // affectation d'une valeur à la variable
 alert(a); // affichage du résultat
 
+/* Calcul de perimètre */
+var rayon = 5.5; // valeur de rayon
+var pi = 3.14; // valeur de pi
+var perimetre = 2 * pi * rayon; // opérations
+alert(perimetre); // affichage du résultat après opérations
+
+
 
